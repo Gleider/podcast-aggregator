4ce543fda1c42852070e4e7917d91f6f7f9e69cf
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-14 12:09:18 -0300

    added the exports of server

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -8,3 +8,5 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 server.listen(port, () => console.log(`Backend is running on port ${port}`))
+
+module.exports = server
