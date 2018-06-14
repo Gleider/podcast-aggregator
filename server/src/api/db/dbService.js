29db465beaf07f988f39249fdee05e73766a1c9c
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-14 12:13:24 -0300

    corrected method error

--- server/src/api/db/dbService.js
+++ server/src/api/db/dbService.js
@@ -1,6 +1,6 @@
 const db = require('./db')
 
-db.methods.methods(['get', 'post', 'put', 'delete'])
+db.methods(['get', 'post', 'put', 'delete'])
 db.updateOptions({ new: true, runValidators: true })
 
 module.exports = db
