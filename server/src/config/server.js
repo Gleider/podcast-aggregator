be8c39d8bda64447f58349ac1023b855b67156c7
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-13 21:36:01 -0300

    server configuration working

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,10 +1,10 @@
-import { urlencoded, json } from 'express';
-import express from 'express';
+const bodyParser = require('body-parser')
+const express = require('express')
 const server = express()
 
 const port = 3003
 
-server.use(urlencoded({ extended: true }))
-server.use(json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 server.listen(port, () => console.log(`Backend is running on port ${port}`))
