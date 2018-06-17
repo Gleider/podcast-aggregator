d1c5475d3ba9921f88c102e518d1a612047e3893
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-16 23:42:44 -0300

    created the users to test

---
+++ server/test/users.js
@@ -0,0 +1,19 @@
+const user1 = {
+  "userName": "Gleider37",
+  "password": "abc1234",
+  "name": "Gleider Mackedanz",
+  "email": "gleider.ec3@gmail.com"
+}
+const user2 = {
+  "userName": "Gleider",
+  "password": "abc1234",
+  "name": "Gleider Mackedanz",
+  "email": "gleider.ec@gmail.com"
+}
+
+const user = []
+user.push(user1)
+user.push(user2)
+
+
+module.exports = user
