edadb4d7649c2a48bff5b7516897b84e8df55c71
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-19 11:47:21 -0300

    added 7 user

--- server/test/users.js
+++ server/test/users.js
@@ -38,7 +38,7 @@ const user3 = {
       "rss": "www.rsslink.com"
     }
   }
-
+  // valid: register complete with two podcast added
   const user5 = {
     "username": "Gleider5",
     "password": "abc1234",
@@ -63,7 +63,7 @@ const user3 = {
     }]
 
   }
-
+  // valid: register complete with one podcast added and one episode
   const user6 = {
     "username": "Gleider6",
     "password": "abc1234",
@@ -87,12 +87,41 @@ const user3 = {
         "url": "www.episodeurl.com"
       }]
     }]
-
   }
   
+  const user7 = {
+    "username": "Gleider7",
+    "password": "abc1234",
+    "name": "Gleider Mackedanz 7",
+    "email": "gleider7@gmail.com",
+    "birth": "1986-07-26",
+    "image": "www.imag.com",
+    "avatar": "www.linktoavatar.com",
+    "about": "informations about me",
+    "podcastSubscribed": [{
+      "name": "podcastname",
+      "description": "this is a podcast about...",
+      "image": "www.linktoimage.com",
+      "url": "www.urltopodcast.com",
+      "rss": "www.rsslink.com",
+      "episodes": [{
+        "title": "episodetitle",
+        "description": "episodedescription",
+        "image": "www.linktoimage.com",
+        "duration": "60min",
+        "url": "www.episodeurl.com"
+      }, {
+        "title": "episodetitle2",
+        "description": "episodedescription2",
+        "image": "www.linktoimage2.com",
+        "duration": "60min",
+        "url": "www.episodeurl2.com"
+      }]
+    }]
+  }
   
 
-const user = [user1, user2, user3, user4, user5, user6]
+const user = [user1, user2, user3, user4, user5, user6, user7]
 
 
 module.exports = user
