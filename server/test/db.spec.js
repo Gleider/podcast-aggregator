9b802e0286fbae138d72386605de25b9abea30c8
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-19 11:46:57 -0300

    passing 8 tests

--- server/test/db.spec.js
+++ server/test/db.spec.js
@@ -102,6 +102,16 @@ describe('User test', () => {
           done()
         })
     })
+
+    it('should send to database a valida register with a podcast and two episodes', (done) => {
+      chai.request(server)
+        .post('/api/db')
+        .send(users[6])
+        .end((err, res) => {
+          res.should.have.status(201)
+          done()
+        })
+    })
   
   })
 
