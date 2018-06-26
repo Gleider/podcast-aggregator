ed14c52ebeea6522c72d6a0cd121905023b80295
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-26 00:01:07 -0300

    passing first test

--- server/test/routes.spec.js
+++ server/test/routes.spec.js
@@ -3,9 +3,9 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = "http://localhost:3003"
-
+//const server = require('../src/config/server')
 const should = chai.should()
-
+//server.close();
 chai.use(chaiHttp)
 
 describe('Routes test', () => {
@@ -15,6 +15,7 @@ describe('Routes test', () => {
         .get('/login')
         .end((err, res) => {
           res.should.have.status(200)
+          res.body.should.be.a('object')
           done()
         })
     })
