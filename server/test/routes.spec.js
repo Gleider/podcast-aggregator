619eb5547c47b6d9de691780427c8a5d2de706e7
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-26 23:45:16 -0300

    modified the test to post login

--- server/test/routes.spec.js
+++ server/test/routes.spec.js
@@ -3,12 +3,18 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = "http://localhost:3003"
-//const server = require('../src/config/server')
+
+const users = require('../src/api/db/db')
+
 const should = chai.should()
-//server.close();
+
 chai.use(chaiHttp)
 
 describe('Routes test', () => {
+  // after(function () {
+  //   server.close();
+  // });
+
   describe('login page', () => {
     it('should get a login page', (done) => {
       chai.request(server)
@@ -16,6 +22,21 @@ describe('Routes test', () => {
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.a('object')
+          done()
+        })
+    })
+    it('should post to login page', (done) => {
+      
+      chai.request(server)
+        .post('/login')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({username:'gleider1'})
+        .end((err, res) => {
+          
+          console.log()
+          res.should.have.status(200)
+          //res.body.should.have.property('username')
+          //res.body.SUCCESS.should.have.property('lastName');
           done()
         })
     })
