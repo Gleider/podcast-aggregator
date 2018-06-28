33123ff6be114afb5523c1e1968fdf83a1b5d7ca
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-27 22:28:05 -0300

    created a new file to routes

---
+++ server/src/api/routes/loginRoute.js
@@ -0,0 +1,20 @@
+const db = require('../db/dbService')
+
+module.exports = {
+    loginGet(req, res, next) {
+    res.send('login page...')
+  },
+
+  loginPost(req, res, next){
+      const user = req.body.username
+      const pass = req.body.password
+
+      db.findOne({username: user}).then(userFind => {
+        if(userFind) {
+          res.send('user finded')
+        } else{
+          res.send('user not finded')
+        }
+      })
+    }
+}
