6f21d0d24a7b70e4de0fe6b3e161da4b75fe0f0c
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-28 22:33:20 -0300

    added token to password

--- server/src/api/routes/loginRoute.js
+++ server/src/api/routes/loginRoute.js
@@ -1,4 +1,9 @@
 const db = require('../db/dbService')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const user = require('./user')
+const env = require('../../.env')
+
 
 module.exports = {
     loginGet(req, res, next) {
@@ -9,12 +14,24 @@ module.exports = {
     const user = req.body.username
     const pass = req.body.password
 
-    db.findOne({username: user}).then(userFind => {
-      if(userFind) {
-        res.send('user finded')
+    db.findOne({ username: user }).then(userFind => {
+      if(userFind && bcrypt.compareSync(pass, userFind.password)) {
+        const token = jwt.sign(userFind, env.authSecret, {
+          expiresIn: "1 day"
+        })
+        const { username } = userFind
+        res.json({ username, token })
       } else{
         res.status(404).json({ username: 'user not found' })
       }
     })
+  },
+
+  validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+    jwt.verify(token, env.authSecret, (err, decoded) {
+      return res.status(200).send({ valid: !err })
+    })
   }
+  
 }
