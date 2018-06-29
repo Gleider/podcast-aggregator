b03351f0da13b16f0262ad2564e1df0418cb76ca
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2018-06-29 03:25:42 +0000

    Refactor UserInfo component

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -7,10 +7,20 @@ export default class UserInfo extends React.Component {
     super(props);
     this.changePassword = this.changePassword.bind(this);
     this.saveChanges = this.saveChanges.bind(this);
+
+    this.state = {
+      passwordDisabled: true
+    }
   }
 
-  changePassword() {
+  changePassword(e) {
+    const newPasswordWrapperClassList = document.getElementById("newPasswordWrapper").classList;
+    this.state.passwordDisabled ? newPasswordWrapperClassList.add("passwordDisabled") : newPasswordWrapperClassList.remove("passwordDisabled");
+    e.target.style.color = this.state.passwordDisabled ? 'inherit' : '#3FC59D';
 
+    this.setState(prevState => ({
+      passwordDisabled: !prevState.passwordDisabled
+    }));
   }
 
   saveChanges() {
@@ -25,7 +35,7 @@ export default class UserInfo extends React.Component {
             <div className="row">
               <div className="form-group col-md-4" id="user-info-image-wrapper">
                 <img src={this.props.image} className="user-info-avatar" alt="Generic User Image" />
-                <button type="submit" className="btn btn-success">Save Changes</button>
+                <button type="submit" className="btn">Save Changes</button>
               </div>
               <div className="col-md-8">
                 <div className="row">
@@ -41,16 +51,16 @@ export default class UserInfo extends React.Component {
                 <div className="form-group row col-md-12">
                   <label>Password</label>
                   <label id="user-info-password">***********</label>
-                  <a><FontAwesomeIcon icon={faPencilAlt} /></a>
+                  <a onClick={this.changePassword} id="editPass"><FontAwesomeIcon icon={faPencilAlt} /></a>
                 </div>
-                <div className="row">
+                <div className="row passwordDisabled" id="newPasswordWrapper">
                   <div className="form-group col-md-6">
                     <label for="newPassword">New Password</label>
-                    <input type="password" className="form-control" id="newPassword" />
+                    <input type="password" className="form-control" id="newPassword" disabled={this.state.passwordDisabled} />
                   </div>
                   <div className="form-group col-md-6">
                     <label for="confirmPassword">Confirm Password</label>
-                    <input type="password" className="form-control" id="confirmPassword" />
+                    <input type="password" className="form-control" id="confirmPassword" disabled={this.state.passwordDisabled} />
                   </div>
                 </div>
               </div>
