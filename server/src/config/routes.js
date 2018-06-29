13d9fd0f4d2ac0d969053a2a226d09e79eee6b80
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-29 20:59:51 -0300

    modified open and protected routes

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -1,83 +1,93 @@
 const express = require('express')
 const login = require('../api/routes/loginRoute')
 const register = require('../api/routes/registerRoute')
+const auth = require('./auth')
 
 module.exports = (server) => {
-  const router = express.Router()
-  const nav = express.Router()
+  /*const router = express.Router()
+  const nav = express.Router()*/
 
-  server.use('/api', router)
-  server.use('', nav)
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
 
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
   
-  const DataBase = require('../api/db/dbService')
-  DataBase.register(router, '/db')
+  protectedApi.use(auth)
 
- 
+  const DataBase = require('../api/db/dbService')
+  DataBase.register(protectedApi, '/db')
+
   // navegation methods
   // login routes
-  nav.route('/login').get(login.loginGet)
-  nav.route('/login').post(login.loginPost)
+  
+  openApi.post('/login', login.loginPost)
+  openApi.get('/login', login.loginGet)
+  // server.route('/login').get(login.loginGet)
+  // server.route('/login').post(login.loginPost)
 
-  // register routes
-  nav.route('/register').get(register.registerGet)
-  nav.route('/register').post(register.registerPost)
-
-  nav.route('/logout').get((req, res, next) => {
+  // // register routes
+  openApi.post('/register', register.registerPost)
+  openApi.get('/register', register.registerGet)
+  // server.route('/register').get(register.registerGet)
+  // server.route('/register').post(register.registerPost)
+  openApi.post('/validateToken', login.validateToken)
+/*
+  server.route('/logout').get((req, res, next) => {
     res.send('logout page')
   })
 
-  nav.route('/user/:id').get((req, res, next) => {
+  server.route('/user/:id').get((req, res, next) => {
     res.send('user page')
   })
 
-  nav.route('/podcast/tags').get((req, res, next) => {
+  server.route('/podcast/tags').get((req, res, next) => {
     res.send('podcast page')
   })
 
-  nav.route('/podcast/tags/:id').get((req, res, next) => {
+  server.route('/podcast/tags/:id').get((req, res, next) => {
     res.send('podcast tag page')
   })
 
-  nav.route('/podcast/:id').get((req, res, next) => {
+  server.route('/podcast/:id').get((req, res, next) => {
     res.send('podcast name page')
   })
 
-  nav.route('/podcast/search').get((req, res, next) => {
+  server.route('/podcast/search').get((req, res, next) => {
     res.send('podcast search page')
   })
 
-  nav.route('/podcast/:podcast/:id').get((req, res, next) => {
+  server.route('/podcast/:podcast/:id').get((req, res, next) => {
     res.send('episode page')
   })
 
-  nav.route('/podcast/top').get((req, res, next) => {
+  server.route('/podcast/top').get((req, res, next) => {
     res.send('top page')
   })
 
   // just logged
 
-  nav.route('/podcast/:id/subscribe').post((req, res, next) => {
+  server.route('/podcast/:id/subscribe').post((req, res, next) => {
     res.send('Subscribe podcast page')
   })
 
-  nav.route('/user/profile').get((req, res, next) => {
+  server.route('/user/profile').get((req, res, next) => {
     res.send('profile page')
   })
 
-  nav.route('/user/profile').put((req, res, next) => {
+  server.route('/user/profile').put((req, res, next) => {
     res.send('profile page')
   })
 
-  nav.route('user/list').get((req, res, next) => {
+  server.route('user/list').get((req, res, next) => {
     res.send('list podcast page')
   })
 
-  nav.route('user/list').put((req, res, next) => {
+  server.route('user/list').put((req, res, next) => {
     res.send('list podcast page')
   })
 
-  nav.route('user/list').delete((req, res, next) => {
+  server.route('user/list').delete((req, res, next) => {
     res.send('list podcast page')
-  })
+  })*/
 }
