82deaa02993cd37441ac79339dc12d1a34cdb9db
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-06-29 19:12:00 -0300

    modified login route

--- server/src/api/routes/loginRoute.js
+++ server/src/api/routes/loginRoute.js
@@ -1,12 +1,12 @@
 const db = require('../db/dbService')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const user = require('./user')
+//const user = require('./user')
 const env = require('../../.env')
 
 
 module.exports = {
-    loginGet(req, res, next) {
+  loginGet(req, res, next) {
     res.send('login page...')
   },
 
@@ -21,17 +21,10 @@ module.exports = {
         })
         const { username } = userFind
         res.json({ username, token })
-      } else{
-        res.status(404).json({ username: 'user not found' })
+      } else {
+        res.status(404).json({ username: 'user or password invalid' })
       }
     })
-  },
-
-  /*validateToken = (req, res, next) => {
-    const token = req.body.token || ''
-    jwt.verify(token, env.authSecret, (err, decoded) {
-      return res.status(200).send({ valid: !err })
-    })
-  }*/
+  }
 
 }
