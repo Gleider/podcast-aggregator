349e6352b96690847b2044429268b4ad02ca647d
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-03 18:27:31 -0300

    modified status return to 403

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -8,11 +8,11 @@ module.exports = (req, res, next) => {
   } else {
     const token = req.body.token || req.query.token || req.headers['authorization']
     if(!token) {
-      return res.status(403).send({ errors: ['No token provided']})
+      return res.status(401).send({ errors: ['No token provided']})
     }
     jwt.verify(token, env.authSecret, (err, decoded) => {
       if(err){
-        return res.status(403).send({
+        return res.status(401).send({
           errors: ['Failed to authenticate token.']
         })
       } else {
