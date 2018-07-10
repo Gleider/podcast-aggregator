206fe3b6fe8411eaf019f0e1e812bc6150c0dcce
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-09 23:11:51 -0300

    created a file with podcasts tests

---
+++ server/test/podcasts.js
@@ -0,0 +1,24 @@
+const podcast1 = {
+  'name':'newpod1',
+  'description':'desc...',
+  'image':'www.image1.com',
+  'url':'www.urlpod.com',
+  'rss':'www.podurl.com',
+}
+
+const podcast2 = {
+  'name':'newpod2',
+  'description':'desc...',
+  'image':'www.image2.com',
+  'url':'www.urlpod.com',
+  'rss':'www.podurl.com',
+}
+
+const episode1 = {
+  'title':'episode1',
+  'description':'a description 1',
+  'image':'www.image.com'
+}
+
+const podcasts = [podcast1, podcast2, episode1]
+module.exports = podcasts
