a4c27d0bedd7dd846299da386b94cc80673e6ff7
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-22 17:44:21 -0300

    sign in user working

--- client/src/pages/SignInForm.js
+++ client/src/pages/SignInForm.js
@@ -8,12 +8,46 @@ class SignInForm extends Component {
     this.state = {
       info: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  componentDidMount(){
-    fetch('/oapi/db')
-      .then(res => res.json())
-      .then(info => this.setState({ info }, () => console.log(info)))
+  // componentDidMount(){
+  //   fetch('/oapi/db')
+  //     .then(res => res.json())
+  //     .then(info => this.setState({ info }, () => console.log(info)))
 
+  // }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const data = {
+      username,
+      password
+    }
+    let formBody = [];
+    for (let property in data) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(data[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    fetch('/oapi/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formBody
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+        console.log("Response:",responseData);
+    })
+    .catch((error) => {
+        console.log('problem while adding data', error);
+    })
   }
   render() {
     return (
@@ -21,8 +55,8 @@ class SignInForm extends Component {
         <form className="FormFields" onSubmit={this.handleSubmit}>
           
         <div class="FormField">
-            <label className="FormField_Label" htmlFor="email">E-Mail Address</label>
-            <input type="email" id="email" className="FormField__Input" placeholder="Enter your e-mail" name="email"/>
+            <label className="FormField_Label" htmlFor="username">Username</label>
+            <input type="text" id="username" className="FormField__Input" placeholder="Enter your e-mail" name="username"/>
           </div>
 
           <div class="FormField">
