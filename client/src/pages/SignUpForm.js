b7d39911ff382e42509785d2be03aae43cb943ae
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-22 17:44:34 -0300

    sign up user working

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -1,22 +1,77 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 class SignUpForm extends Component {
+  constructor(){
+    super();
+    this.state = {
+      info: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+    const name = e.target.name.value;
+    const email = e.target.email.value
+    const data = {
+      username,
+      email,
+      name,
+      password,
+      confirmPassword
+    }
+    let formBody = [];
+    for (let property in data) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(data[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    fetch('/oapi/register', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formBody
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+        console.log("Response:",responseData);
+    })
+    .catch((error) => {
+        console.log('problem while adding data', error);
+    })
+  }
   render() {
     return (
       <div className="FormCenter">
         <form className="FormFields" onSubmit={this.handleSubmit}>
           <div class="FormField">
+            <label className="FormField_Label" htmlFor="username">Username</label>
+            <input type="username" id="username" className="FormField__Input" placeholder="Enter your username" name="username"/>
+          </div>
+          <div class="FormField">
             <label className="FormField_Label" htmlFor="name">Full Name</label>
             <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+          </div>
+          <div class="FormField">
+            <label className="FormField_Label" htmlFor="email">E-Mail Address</label>
+            <input type="email" id="email" className="FormField__Input" placeholder="Enter your e-mail" name="email"/>
           </div>
           <div class="FormField">
             <label className="FormField_Label" htmlFor="password">Password</label>
             <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password"/>
           </div>
           <div class="FormField">
-            <label className="FormField_Label" htmlFor="email">E-Mail Address</label>
-            <input type="email" id="email" className="FormField__Input" placeholder="Enter your e-mail" name="email"/>
+            <label className="FormField_Label" htmlFor="confirmPassword">Repeat password</label>
+            <input type="password" id="confirmPassword" className="FormField__Input" placeholder="Repeat your password" name="confirmPassword"/>
           </div>
+          
 
           <div className="FormField">
             <label className="FormField__CheckboxLabel">
