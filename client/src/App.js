f3c335e8f09a422e228d52fdc700ed5a7ff02db4
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-23 21:55:15 -0300

    created initial animation arrow

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
+import './pages/css/animateArrow.css'
+import Menu from './pages/Menu';
+import AnimationArrow from './pages/AnimateArrow';
 import './App.css';
-
+  //TRYING MAKE WORK THE ANIMATION ARROW AND USE THIS TUTORIAL TO FINISHER: https://www.youtube.com/watch?time_continue=48&v=IGz4BI-aO_8
 class App extends Component {
   state = {
     response: ''
@@ -14,9 +17,15 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <div className="App__Aside"></div>
+          <div className="App__Aside">
+            <Route path='/' component={Menu}></Route>
+
+          </div>
+          
           <div className="App__Form">
+          <Route path="/" component={AnimationArrow}></Route>
             <div className="PageSwitcher">
+              
               <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
               <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
             </div>
@@ -26,6 +35,8 @@ class App extends Component {
               <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
             </div>
 
+            
+            
             <Route exact path="/" component={SignUpForm}></Route>
             <Route path="/sign-in" component={SignInForm}></Route>
 
---
+++ client/src/pages/AnimateArrow.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './css/animateArrow.css'
+
+class AnimateArrow extends Component {
+  render(){
+    return (
+      <span className="arrow"></span>
+    );
+  }
+}
+ 
+export default AnimateArrow;
