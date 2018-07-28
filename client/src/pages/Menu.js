d9a250ef3d907810e87649ccb845c005f8200a65
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-07-28 18:00:53 -0300

    created the first version of menu

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react'
-import './css/menu.css'
+import MenuConfig from './MenuConfig'
+import logo from './images/logo.png';
 class Menu extends Component {
-  render(){
-    return (
-      <div className="container center">
-        <nav className="menu">
-          <h1 className="menu__logo">Epic co.</h1>
-
-
-          <div className="menu__right">
-            <ul className="menu__list">
-              <li className="menu__list-item"><a className="menu__link--active" href="#">Home</a></li>
-              <li className="menu__list-item"><a className="menu__link--active" href="#">About</a></li>
-              <li className="menu__list-item"><a className="menu__link--active" href="#">Portifolio</a></li>
-              <li className="menu__list-item"><a className="menu__link--active" href="#">Contact</a></li>
-              
-            </ul>
-            <button className="menu__search-button"></button>
-
-            <form className="menu__search-form hide" method="POST">
-              <input className="menu__search-input" placeholder="Type and hit enter" />
-            </form>
-          </div>
-        </nav>
-      </div>
-    )
-  }
+    render() {
+      let links = [
+        { label: 'Home', link: '#home', active: true },
+        { label: 'About', link: '#about' },
+        { label: 'Portfolio', link: '#portfolio' },
+        { label: 'Contact Us', link: '#contact-us' },
+      ];
+  
+      return (
+        <div className="container center">
+          <MenuConfig links={links} logo={logo} />
+        </div>
+      );
+    }
 }
 
 export default Menu;
